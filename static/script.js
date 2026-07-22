2bfccb6588a3cfd72e345a059b8173dfa82a80c7
Medha <medhamalliraj@gmail.com>
2026-07-22 22:37:58 +0530

    Fix dark mode toggle

--- static/script.js
+++ static/script.js
@@ -370,19 +370,39 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
     // ==========================
-    // Theme Toggle
-    // ==========================
+// Theme Toggle
+// ==========================
+
+const themeBtn = document.getElementById("themeToggle");
 
 
-    document
-    .getElementById("themeToggle")
-    ?.addEventListener("click",()=>{
+if(localStorage.getItem("theme") === "dark"){
+
+    document.body.classList.add("dark-mode");
+
+}
 
 
-        document.body.classList.toggle("dark");
+themeBtn?.addEventListener("click",()=>{
 
 
-    });
+    document.body.classList.toggle("dark-mode");
+
+
+    if(document.body.classList.contains("dark-mode")){
+
+        localStorage.setItem("theme","dark");
+
+    }
+
+    else{
+
+        localStorage.setItem("theme","light");
+
+    }
+
+
+});
 
 
 
